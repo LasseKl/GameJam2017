f273d45075229c139b6cb0c2282c025a7bef668e
Dario D. Müller <mailme@dariodomi.de>
2017-10-28 01:21:25 +0200

    CHANGED Room

--- Assets/Scripts/Gameplay/Room.cs
+++ Assets/Scripts/Gameplay/Room.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public Transform Transform;
 
     void Start()
     {
+        Transform = this.GetComponent<Transform>();
         Config.Instance.Rooms.Add(this);
     }
 }

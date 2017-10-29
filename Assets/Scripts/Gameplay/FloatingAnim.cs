3613da6c8aba8dd6c1f259820f6878cb55d757a1
LasseKl <lassekluever@web.de>
2017-10-29 04:58:16 +0100

    ADDED New Ghost

---
+++ Assets/Scripts/Gameplay/FloatingAnim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FloatingAnim : MonoBehaviour
+{
+    const float SPEED = 0.3f;
+
+    public float maxY;
+    public float minY;
+
+    public bool goUp = false;
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + (goUp ? SPEED * Time.deltaTime : SPEED * -Time.deltaTime), transform.position.z);
+
+        if (transform.position.y <= minY)
+            goUp = true;
+        if (transform.position.y >= maxY)
+            goUp = false;
+    }
+}
